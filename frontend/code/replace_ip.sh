94145050a25710cb44497b47c517c929e7951680
drevsmart <drevsmart@gmail.com>
2021-08-05 14:19:06 +0000

    beta_v1

--- frontend/code/replace_ip.sh
+++ frontend/code/replace_ip.sh
@@ -4,6 +4,4 @@ echo $ADDRESS
 
 {
     grep 'localhost' index.html | xargs sed -i 's/localhost/'$ADDRESS'/g' index.html
-} 
-
-&> /dev/null
+}&> /dev/null
--- frontend/get_ip.sh
+++
@@ -1,4 +0,0 @@
-ADDRESS=$(wget -O - -q icanhazip.com)
-
-grep './index.html' -P -R -I -l  * | xargs sed -i 's/localhost/ip='$ADDRESS'/g'
-echo done
